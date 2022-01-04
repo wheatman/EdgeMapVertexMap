2162abda5cc0ba3ca1c33a5e11732243ca20f361
Brian Wheatman <bwheatman@gmail.com>
2022-01-04 16:38:37 -0500

    adding AdjacencyVector example

---
+++ AdjacencyVector/CSR.cpp
@@ -0,0 +1,130 @@
+/*
+ * adjacency matrix
+ */
+
+#include <algorithm>
+#include <cassert>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iterator>
+#include <limits>
+#include <vector>
+
+#include "../io_util.hpp"
+
+#include "../algorithms/BC.h"
+#include "../algorithms/BFS.h"
+#include "../algorithms/Components.h"
+#include "../algorithms/PageRank.h"
+
+template <class node_t> class AdjacencyVector {
+  // data members
+  std::vector<std::vector<node_t>> nodes;
+
+public:
+  // function headings
+
+  AdjacencyVector(node_t n, std::vector<std::pair<node_t, node_t>> edges_list)
+      : nodes(n) {
+    std::sort(edges_list.begin(), edges_list.end());
+    auto new_end = std::unique(edges_list.begin(), edges_list.end());
+    edges_list.resize(std::distance(edges_list.begin(), new_end));
+    for (const auto &e : edges_list) {
+      nodes[e.first].push_back(e.second);
+    }
+  }
+
+  size_t num_nodes() const { return nodes.size(); }
+
+  void *getExtraData() const { return nullptr; }
+
+  template <class F>
+  void map_neighbors(node_t node, F f, [[maybe_unused]] void *d,
+                     bool parallel) const {
+
+    if (parallel) {
+      parallel_for(size_t i = 0; i < nodes[node].size(); i++) {
+        f.update(node, nodes[node][i]);
+      }
+    } else {
+      for (size_t i = 0; i < nodes[node].size(); i++) {
+        f.update(node, nodes[node][i]);
+      }
+    }
+  }
+};
+
+int main(int32_t argc, char *argv[]) {
+  if (argc < 3) {
+    printf("call with graph filename, and which algorithm to run, and "
+           "optionally the start node\n");
+    return 0;
+  }
+  std::string graph_filename = std::string(argv[1]);
+
+  uint64_t edge_count;
+  uint32_t node_count;
+  auto edges =
+      get_edges_from_file_adj_sym(graph_filename, &edge_count, &node_count);
+  AdjacencyVector<uint32_t> g = AdjacencyVector<uint32_t>(node_count, edges);
+  std::string algorithm_to_run = std::string(argv[2]);
+  if (algorithm_to_run == "bfs") {
+    uint64_t source_node = std::strtol(argv[3], nullptr, 10);
+    int32_t *bfs_out = BFS(g, source_node);
+    std::vector<uint32_t> depths(node_count,
+                                 std::numeric_limits<uint32_t>::max());
+    parallel_for(uint32_t j = 0; j < node_count; j++) {
+      uint32_t current_depth = 0;
+      int32_t current_parent = j;
+      if (bfs_out[j] < 0) {
+        continue;
+      }
+      while (current_parent != bfs_out[current_parent]) {
+        current_depth += 1;
+        current_parent = bfs_out[current_parent];
+      }
+      depths[j] = current_depth;
+    }
+    std::ofstream myfile;
+    myfile.open("bfs.out");
+    for (unsigned int i = 0; i < node_count; i++) {
+      myfile << depths[i] << std::endl;
+    }
+    myfile.close();
+    free(bfs_out);
+  }
+  if (algorithm_to_run == "bc") {
+    uint64_t source_node = std::strtol(argv[3], nullptr, 10);
+    double *bc_out = BC(g, source_node);
+    std::ofstream myfile;
+    myfile.open("bc.out");
+    for (unsigned int i = 0; i < node_count; i++) {
+      myfile << bc_out[i] << std::endl;
+    }
+    myfile.close();
+    free(bc_out);
+  }
+  if (algorithm_to_run == "pr") {
+    uint64_t iters = std::strtol(argv[3], nullptr, 10);
+    double *pr_out = PR_S<double>(g, iters);
+    std::ofstream myfile;
+    myfile.open("pr.out");
+    for (unsigned int i = 0; i < node_count; i++) {
+      myfile << pr_out[i] << std::endl;
+    }
+    myfile.close();
+    free(pr_out);
+  }
+  if (algorithm_to_run == "cc") {
+    uint32_t *cc_out = CC(g);
+    std::ofstream myfile;
+    myfile.open("cc.out");
+    for (unsigned int i = 0; i < node_count; i++) {
+      myfile << cc_out[i] << std::endl;
+    }
+    myfile.close();
+    free(cc_out);
+  }
+}
